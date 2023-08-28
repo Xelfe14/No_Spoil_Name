ccf8c856f6494c7ed06c1784a807b0165e1d5bd5
chenzhangzhao <donconfucian@gmail.com>
2023-08-28 17:56:59 +1000

    text to chapters

---
+++ no_spoil_zone/utils/chapters.py
@@ -0,0 +1,21 @@
+import re
+
+def get_chapter(book_name, chapter_number):
+    
+    filepath = book_name+'.txt'
+    with open(filepath, "r", encoding="utf8") as file:
+        text = file.read()
+        text = text.replace('\n',' ')
+
+    if len(re.findall('chapter',text,flags = re.IGNORECASE)) != 0:
+
+        chapters = re.split("chapter ", text, flags = re.IGNORECASE)
+        num = chapter_number+1
+
+    else:
+
+        return None:
+
+    return chapters[num]
+
+get_chapter("A Boy's Fortune; Or, The Strange Adventures of Ben Baker",1)
