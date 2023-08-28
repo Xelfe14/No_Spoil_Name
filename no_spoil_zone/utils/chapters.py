d1c83cd0879a81042e5c0f29f61d19b9f2a707e7
chenzhangzhao <139095738+chenzhangzhao@users.noreply.github.com>
2023-08-28 18:04:56 +1000

    Update chapters.py

--- no_spoil_zone/utils/chapters.py
+++ no_spoil_zone/utils/chapters.py
@@ -5,7 +5,7 @@ def get_chapter(book_name, chapter_number):
     filepath = book_name+'.txt'
     with open(filepath, "r", encoding="utf8") as file:
         text = file.read()
-        text = text.replace('\n',' ')
+    text = text.replace('\n',' ')
 
     if len(re.findall('chapter',text,flags = re.IGNORECASE)) != 0:
 
