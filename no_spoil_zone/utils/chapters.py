2bc0e07bfd4200d3ba41ac508849cd7fe7dc0957
chenzhangzhao <139095738+chenzhangzhao@users.noreply.github.com>
2023-08-29 18:24:00 +1000

    Update chapters.py

--- no_spoil_zone/utils/chapters.py
+++ no_spoil_zone/utils/chapters.py
@@ -20,7 +20,7 @@ def get_chapter(book_name, chapter_number):
 
     if case1 and case2:
         print('case1&2')
-        matches=[match.span() for match in re.finditer('chapter',text,flags=re.IGNORECASE)]
+        matchesone=[match.span()[0] for match in re.finditer('chapter',text,flags=re.IGNORECASE)]
         fi_ch = 0
         for i in range(1,len(matchesone)):
             if matchesone[i]-matchesone[i-1]<50:
@@ -41,7 +41,7 @@ def get_chapter(book_name, chapter_number):
 
     elif case2:
 #         print('case2')
-        matches=[match.span() for match in re.finditer('chapter',text,flags=re.IGNORECASE)]
+        matchesone=[match.span()[0] for match in re.finditer('chapter',text,flags=re.IGNORECASE)]
         fi_ch = 0
         for i in range(1,len(matchesone)):
             if matchesone[i]-matchesone[i-1]<50:
@@ -57,4 +57,4 @@ def get_chapter(book_name, chapter_number):
     else:
         return None
 
-# get_chapter("Wait and Hope; Or, A Plucky Boy's Luck",3)
+result = get_chapter("Wait and Hope; Or, A Plucky Boy's Luck",5)
