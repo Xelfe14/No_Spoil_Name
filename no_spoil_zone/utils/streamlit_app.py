851f831420c7bf3978147ff44d4107dd6e2c1785
taddeocarpinelli <taditaliano@gmail.com>
2023-09-07 09:14:25 +0800

    building the app

--- no_spoil_zone/utils/streamlit_app.py
+++ no_spoil_zone/utils/streamlit_app.py
@@ -32,83 +32,75 @@ page_bg_img = f"""
 </style>
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-#Front header
-st.markdown(
-        "<h1 style='text-align: center; color: black;'>💯 No Sploil Zone! 💯</h1>",
+
+def main():
+    st.markdown(
+        "<h1 style='text-align: center; color: white;'>💯 No Sploil Zone! 💯</h1>",
         unsafe_allow_html=True
     )
-st.markdown("### 📚 Choose a Book and Discover Its Summary and Topics 📚")
-st.markdown("---")
-
-with st.container():
-    # User input
-    author_name = st.selectbox("Select Author", ["Jane Austen", "Shakespeare, William", "Twain, Mark"])
-    book_name = st.selectbox("Select Book", ["Emma", "Hamlet", "Tom Sawyer"])
-    chapter_number = int(st.text_input('Total Number of Chapters'))
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-# PARAMS
-
-dummy_dict={
-    'master_sum': 'sumssunfovnzonzrnojvoizjfoznocnzoecizecinzoiczopejozjfojaofjaozj)ajfàjaoiefjoiejfoijfoinekon'
-    }
-dummy_dict_2={
-    "chapter_sum": 'jfpefpzkfoz,foz,io,zf,zofnozfioze,foz,eiof,zepfk,pze,',
-    "topics": {
-        "persons" :['j','o','r'],
-        "places": ['a','b','h'],
-        "general": ['x','z','w']
-        }
-    }
-params = {
-    'author_name': author_name,
-    'book_name': book_name,
-    'chapter': chapter_number
-    }
-url_chapter_summary='https://api-no-spoil-zone-vr5zz4u7ca-uc.a.run.app/master_summary?author_name=ane%20Austen&book_name=Emma&chapter_number=2'
-
-# url_main_summary=f'https://api-no-spoil-zone-vr5zz4u7ca-uc.a.run.app/master_summary?author_name={author_name}&book_name={book_name}&chapter_number={chapter_number}'
-url_main_summary= 'https://api-no-spoil-zone-vr5zz4u7ca-uc.a.run.app/chapter_summary?author_name=Jane%20Austen&book_name=Emma&chapter_number=2'
-main_summary_response= requests.get(url_main_summary).json()
-chapter_summary_response= requests.get(url_chapter_summary).json()
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-if st.button("Run Summarization 🏄🏼"):
-    st.spinner('Running! No sploils here :😉:')
-    #-------------------------------------------------------------------------------------------------------------------------------------------------------
-    #Main Summary
-
-    if chapter_number == 1:
-        st.header(f"🧐💭 What happens in the first chapter of {book_name}?")
-        st.markdown(main_summary_response['master_sum'])
-    else:
-        st.header(f"🧐💭 What happens in the first: {chapter_number} chapters of {book_name}?")
-        st.markdown(main_summary_response['master_sum'])
-        st.markdown("###---")
-
-
-    # if book_summary_reponse.status_code == 200:
-    #     book_summary = book_summary_reponse.json()
-    #     st.write(book_summary)
-    # else:
-    #     st.write("Error retrieving summary")
-
-    #-------------------------------------------------------------------------------------------------------------------------------------------------------
-    # #Summary Breakdown
-
-    st.title("Chapter Breakdown")
-    for i in range(chapter_number):
-        # Display topics for the current chapter
-        st.markdown(f"### Topics for Chapter {i + 1}")
-        for topic, value in chapter_summary_response.items():
-            st.write(f"**{topic}**: {', '.join(value)}")
-
-         # Display the summary for the current chapter
-        st.markdown(f"### Summary for Chapter {i + 1}")
-        st.write(chapter_summary_response['chapter_sum'])
-
-    # Add a horizontal line to separate chapters
+​
+    st.markdown("### 📚 Choose a Book and Discover Its Summary and Topics 📚")
     st.markdown("---")
+​
+​
+    col1, col2, col3 = st.columns(3)
+​
+    with col1:
+        author_name = st.selectbox("Select Author", ["G. A. Henty", "Henri Rene Guy de Maupassant", "Honore de Balzac", "Jane Austen"])
+    with col2:
+        book_name = st.selectbox("Select Book", ["Emma", "Hamlet", "Tom Sawyer"])
+    with col3:
+        chapter_number = st.number_input("Enter Chapter Number:", min_value=1, value=1)
+​
+    st.markdown("---")
+​
+    if st.button("Run Summarization 🚀"):
+        with st.spinner('Running! No sploils here 😉'):
+            try:
+                # Making API request for Master Summary
+                master_url = f"https://api-no-spoil-zone-vr5zz4u7ca-uc.a.run.app/"
+​
+                master_summary_response = requests.get(f"{master_url}master_summary?author_name={author_name}&book_name={book_name}&chapter_number={chapter_number}")
+​
+                if master_summary_response.status_code == 200:
+                    master_summary_data = master_summary_response.json()
+                    # Assuming the summary is returned in 'summary' key in JSON response
+                    master_summary = master_summary_data.get('master_sum', '')
+​
+                    if chapter_number == 1:
+                       st.subheader(f"🤔💭 What happens in the first chapter of {book_name}?")
+                    else:
+                        st.subheader(f"🤔💭 What happens in the first {chapter_number} chapters of {book_name}?")
+                    st.markdown(master_summary)
+​
+                # else:
+                #     st.error(f"Failed to get Master Summary. Status Code: {master_summary_response.status_code}")
+​
+                st.markdown("---")
+​
+                # Similarly, you can make API requests for individual chapter summaries here.
+                for i in range(1, chapter_number + 1):
+                    chapter_summary_response = requests.get(f"{master_url}chapter_summary?author_name={author_name}&book_name={book_name}&chapter_number={i}")
+                    if chapter_summary_response.status_code == 200:
+                        chapter_summary_data = chapter_summary_response.json()
+                        chapter_summary = chapter_summary_data.get('chapter_sum', '')
+                        topic_dict = chapter_summary_data.get('topics', '')
+                        st.subheader(f"Chapter {i} - People, Places, and Things")
+                        st.write("========================================================================================")
+                        st.markdown(f"**People:** {', '.join(topic_dict['persons'])}")
+                        st.markdown(f"**Places:** {', '.join(topic_dict['places'])}")
+                        st.markdown(f"**Things:** {', '.join(topic_dict['general'])}")
+                        st.subheader(f"Chapter {i} - Summary")
+                        st.write("========================================================================================")
+                        st.markdown(chapter_summary)
+                    else:
+                        st.subheader(f"Chapter {i} Summary")
+                        st.markdown("Not found")
+​
+​
+​
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+​
+if __name__ == '__main__':
+    main()
