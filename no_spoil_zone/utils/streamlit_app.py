7aa6d90f96385a522adee489494e12ce55f21987
taddeocarpinelli <taditaliano@gmail.com>
2023-09-07 11:01:57 +0800

    building the app

--- no_spoil_zone/utils/streamlit_app.py
+++ no_spoil_zone/utils/streamlit_app.py
@@ -6,39 +6,15 @@ import base64
 import plotly.express as px
 import requests
 
-# Background image CSS
-# page_bg_img = f"""
-# <style>
-# [data-testid="stAppViewContainer"] > .main {{
-#     background-image: url("https://img.freepik.com/premium-photo/illustration-magic-book-with-fantastic-stories-ai-generated_871188-653.jpg");
-#     background-size: 100%;
-#     background-position: center;
-#     background-repeat: no-repeat;
-#     background-attachment: local;
-# }}
-
-# [data-testid="stHeader"] {{
-#     background:
-# }}
-
-# [data-testid="stToolbar"] {{
-#     right: 2rem;
-# }}
-
-# .css-1n76uvr .e1tzin5v0 {{
-#   background: rgba(255,255,255,0.5)
-# }}
-
-# </style>
-# """
-# st.markdown(page_bg_img, unsafe_allow_html=True)
-
 def main():
     st.markdown(
-        "<h1 style='text-align: center; color: white;'>💯 No Sploil Zone! 💯</h1>",
+        "<h1 style='text-align: center; color: #E31727 ;'>:100: No Sploil Zone! :100:</h1>",
         unsafe_allow_html=True
     )
-    st.markdown("### 📚 Choose a Book and Discover Its Summary and Topics 📚")
+    st.markdown(
+        "<div style='text-align: center;'>knowledge now, spoils later :chut:</div>",
+        unsafe_allow_html=True
+    )
     st.markdown("---")
     author_book_dict = {
         "G. A. Henty": ["A Knight of the White Cross: A Tale of the Siege of Rhodes", "All But Lost: A Novel. Vol. 2 of 3",\
@@ -52,65 +28,53 @@ def main():
         "Henri Rene Guy de Maupassant": ["Yvette"],
         "Honore de Balzac": ["A Daughter of Eve"],
         "Jane Austen": ["Emma"]
-        }
+    }
     col1, col2, col3 = st.columns(3)
-
     with col1:
         author_name = st.selectbox("Select Author", ["G. A. Henty", "Henri Rene Guy de Maupassant", "Honore de Balzac", "Jane Austen"])
     with col2:
         book_name = st.selectbox("Select Book", author_book_dict[author_name])
     with col3:
         chapter_number = st.number_input("Enter Chapter Number:", min_value=1, value=1)
-
     st.markdown("---")
-
-    if st.button("Run Summarization 🚀"):
-        with st.spinner('Running! No sploils here 😉'):
+    if st.button("Run Summarization :fusée:"):
+        with st.spinner('Running! No sploils here :clin_d'œil:'):
             try:
                 # Making API request for Master Summary
                 master_url = f"https://api-no-spoil-zone-vr5zz4u7ca-uc.a.run.app/"
-
                 master_summary_response = requests.get(f"{master_url}master_summary?author_name={author_name}&book_name={book_name}&chapter_number={chapter_number}")
-
                 if master_summary_response.status_code == 200:
                     master_summary_data = master_summary_response.json()
                     # Assuming the summary is returned in 'summary' key in JSON response
                     master_summary = master_summary_data.get('master_sum', '')
-
                     if chapter_number == 1:
-                       st.subheader(f"🤔💭 What happens in the first chapter of {book_name}?")
+                       st.subheader(f":pensif::bulle_de_pensée: What happens in the first chapter of {book_name}?")
                     else:
-                        st.subheader(f"🤔💭 What happens in the first {chapter_number} chapters of {book_name}?")
+                        st.subheader(f":pensif::bulle_de_pensée: What happens in the first {chapter_number} chapters of {book_name}?")
                     st.markdown(master_summary)
-
                 # else:
                 #     st.error(f"Failed to get Master Summary. Status Code: {master_summary_response.status_code}")
-
                 st.markdown("---")
-
-               # Similarly, you can make API requests for individual chapter summaries here.
+                # Similarly, you can make API requests for individual chapter summaries here.
                 for i in range(1, chapter_number + 1):
                     chapter_summary_response = requests.get(f"{master_url}chapter_summary?author_name={author_name}&book_name={book_name}&chapter_number={i}")
                     if chapter_summary_response.status_code == 200:
                         chapter_summary_data = chapter_summary_response.json()
                         chapter_summary = chapter_summary_data.get('chapter_sum', '')
                         topic_dict = chapter_summary_data.get('topics', '')
-                        st.subheader(f"Chapter {i} - People, Places, and Things")
-                        st.write("========================================================================================")
+                        st.subheader(f"Chapter {i}")
+                        st.write("=========================================================================")
+                        st.subheader("People :homme_femme_fille_garçon:, Places :immeubles:, and Things :pion_échec:")
                         st.markdown(f"**People:** {', '.join(topic_dict['persons'])}")
                         st.markdown(f"**Places:** {', '.join(topic_dict['places'])}")
                         st.markdown(f"**Things:** {', '.join(topic_dict['general'])}")
-                        st.subheader(f"Chapter {i} - Summary")
-                        st.write("========================================================================================")
+                        st.subheader(f"Summary :bloc_notes:")
                         st.markdown(chapter_summary)
+                        st.markdown("---")
                     else:
                         st.subheader(f"Chapter {i} Summary")
                         st.markdown("Not found")
-
-
-
             except Exception as e:
                 st.error(f"An error occurred: {e}")
-
 if __name__ == '__main__':
     main()
